715c53801c5caaafe34da778c913e40f7e3227ba
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-10 23:07:52 -0300

    contact component with icons (not finished)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,16 @@
+import Image from "next/image"
+import styles from "./Contact.module.scss"
+
 export default function Contact() {
-    return <>
-        <h2>este es el componente de contacto</h2></>
+    return <div className={styles.container}>
+        <a href="mailto:gdbarimboim@gmail.com">
+            <Image src={"/envelope-plus-fill.svg"} alt="email" width={40} height={40} className={styles.icons} />
+        </a>
+        <a href="linkedin">
+            <Image src={"/linkedin.svg"} alt="linkedin" width={40} height={40} className={styles.icons}/>
+        </a>
+        <a href="github">
+            <Image src={"/github.svg"} alt="github" width={40} height={40} className={styles.icons}/>
+        </a>
+    </div>
 }
