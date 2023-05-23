ac1d9dd1d6ee18db68c9f6f2210eb53b7bc598bb
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-23 12:58:20 -0300

    home grid ready

--- src/components/home/homeComponent.tsx
+++ src/components/home/homeComponent.tsx
@@ -2,17 +2,20 @@ import Image from 'next/image'
 import styles from "./homeComponent.module.scss"
 
 export default function HomeComponent() {
-    return <div>
-        <div>
+    return <div className={styles.grid}>
+        <div className={styles.element1}>
+            <Image className={styles.arrow} src={"/Arrow.png"} alt='arrow' width={100} height={120} />
+        </div>
+        <div className={styles.element2}>
             <Image src={"/imageForHome.svg"} alt='imgHome' width={500} height={500} />
         </div>
-        <div>
-            <p>Hola! Soy Gabriel Barimboim</p>
+        <div className={styles.element3}>
+            <p>Hola! Soy <span className={styles.name}>Gabriel Barimboim</span></p>
         </div>
-        <div>
-            <p>Un Desarrollador Web</p>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dicta, officia adipisci quisquam iure soluta dolor pariatur hic placeat. Excepturi vitae tempore nihil quisquam architecto odit magnam repellendus dolore ullam!</h2>
-            <p>Lorem, ipsum dolor </p>
+        <div className={styles.element4}>
+            <p className={styles.nestedElement1}>Un Desarrollador Web</p>
+            <h2 className={styles.nestedElement2}>Lorem ipsum dolor sit amet.</h2>
+            <p className={styles.nestedElement3}>Lorem, ipsum dolor </p>
         </div>
     </div>
 }
