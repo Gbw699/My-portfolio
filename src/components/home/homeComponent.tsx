c6c8be9c3ecde1616678467ab4a2d5a8c26bc4fc
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-23 13:29:43 -0300

    added radial gradient

--- src/components/home/homeComponent.tsx
+++ src/components/home/homeComponent.tsx
@@ -4,10 +4,10 @@ import styles from "./homeComponent.module.scss"
 export default function HomeComponent() {
     return <div className={styles.grid}>
         <div className={styles.element1}>
-            <Image className={styles.arrow} src={"/Arrow.png"} alt='arrow' width={100} height={120} />
+            <Image className={styles.arrow} src={"/Arrow.png"} alt='arrow' width={150} height={170} />
         </div>
         <div className={styles.element2}>
-            <Image src={"/imageForHome.svg"} alt='imgHome' width={500} height={500} />
+            <Image src={"/imageForHome.svg"} alt='imgHome' width={350} height={350} />
         </div>
         <div className={styles.element3}>
             <p>Hola! Soy <span className={styles.name}>Gabriel Barimboim</span></p>
