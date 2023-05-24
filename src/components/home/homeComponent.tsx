bdfc28b9cdb16980f4a7a9bf995ce60c8590eb85
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-24 13:09:35 -0300

    home component ready

--- src/components/home/homeComponent.tsx
+++ src/components/home/homeComponent.tsx
@@ -1,21 +1,28 @@
 import Image from 'next/image'
 import styles from "./homeComponent.module.scss"
+import { Gloria_Hallelujah } from 'next/font/google'
+
+const gloria_hallelujah = Gloria_Hallelujah({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+})
 
 export default function HomeComponent() {
-    return <div className={styles.grid}>
+    return <div className={`${styles.grid} ${gloria_hallelujah.className}`}>
         <div className={styles.element1}>
-            <Image className={styles.arrow} src={"/Arrow.png"} alt='arrow' width={150} height={170} />
+            <Image className={styles.arrow} src={"/Arrow.png"} alt='arrow' width={122} height={118} />
         </div>
         <div className={styles.element2}>
             <Image src={"/imageForHome.svg"} alt='imgHome' width={350} height={350} />
         </div>
         <div className={styles.element3}>
-            <p>Hola! Soy <span className={styles.name}>Gabriel Barimboim</span></p>
+            <p>Hola! Soy <a href='https://linktr.ee/gbw699' target='_blank' className={styles.name}>Gabriel Barimboim</a></p>
         </div>
         <div className={styles.element4}>
             <p className={styles.nestedElement1}>Un Desarrollador Web</p>
-            <h2 className={styles.nestedElement2}>Que convierte café en código</h2>
-            <p className={styles.nestedElement3}>Lorem, ipsum dolor </p>
+            <h2 className={styles.nestedElement2}>Que convierte mate en <a href='https://github.com/Gbw699/My-portfolio' target='_blank' className={styles.text}>código</a>...</h2>
+            <p className={styles.nestedElement3}>Y en buenos momentos :) </p>
         </div>
     </div>
 }
