c30723439aa648acbfdd6ce550f45e21ff25e3c6
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-24 16:54:38 -0300

    font added to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,13 @@
+import { Gloria_Hallelujah } from 'next/font/google'
+
+const gloria_hallelujah = Gloria_Hallelujah({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+})
+
 export default function About() {
-    return <>
-    <h2>about page</h2>
-    </>
+    return <div className={`${gloria_hallelujah.className}`}>
+        <h1>Sobre mi</h1>
+    </div>
 }
