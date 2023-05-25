f56b204e098bbe6f3c0901a2df5213cc49198002
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-25 19:20:47 -0300

    about text styles created

--- src/components/aboutText/AboutText.tsx
+++ src/components/aboutText/AboutText.tsx
@@ -1,8 +1,18 @@
-export default function AboutText () {
+import styles from "./AboutText.module.scss"
+
+export default function AboutText() {
     return <div>
-        <h1>Soy desarrollador Full Stack...</h1>
-        <p>Todo empezó el día que decidí dejar de estudiar producción audiovisual. La pregunta clave en ese momento fue ¿Me veo viviendo del cine en un futuro?. Podría decir que la respuesta corta fue un {'"NO"'}, pero la realidad siempre es un poco más compleja. La respuesta larga fue {'"SÍ"'} quiero seguir viviendo del cine, pero {'"NO"'} me veo viviendo del cine.</p>
-        <p>En medio de semejante contradicción (que incluso suena incoherente) fue que decidí probar con la programación, aunque no fue una decisión enteramente propia. La verdad es que en un principio yo fuí el típico caso de {'"me anotó un amigo"'} (en mi caso 3) que me insistieron para que le diera una oportunidad. Ya ha pasado 1 año desde ese momento y puedo decir que mis amigos no estaban equivocados al insistirme.</p>
-        <p>La programación ha sido la pieza clave del rompecabezas. Descubrí que la programación es algo que me encanta (principalmente el front) y que me va a dar la posibilidad de vivir el cine como hobby, sin tener que vivir del cine laboralmente.   </p>
+        <h1 className={styles.title}>
+            Soy desarrollador Full Stack...
+        </h1>
+        <p className={styles.text}>
+            Todo empezó el día que decidí dejar de estudiar producción audiovisual. La pregunta clave en ese momento fue ¿Me veo viviendo del cine en un futuro?. Podría decir que la respuesta corta fue un {'"NO"'}, pero la realidad siempre es un poco más compleja. La respuesta larga fue {'"SÍ"'} quiero seguir viviendo del cine, pero {'"NO"'} me veo viviendo del cine.
+            <br />
+            <br />
+            En medio de semejante contradicción (que incluso suena incoherente) fue que decidí probar con la programación, aunque no fue una decisión enteramente propia. La verdad es que en un principio yo fuí el típico caso de {'"me anotó un amigo"'} (en mi caso 3) que me insistieron para que le diera una oportunidad. Ya ha pasado 1 año desde ese momento y puedo decir que mis amigos no estaban equivocados al insistirme.
+            <br /> 
+            <br /> 
+            La programación ha sido la pieza clave del rompecabezas. Descubrí que la programación es algo que me encanta (principalmente el front) y que me va a dar la posibilidad de vivir el cine como hobby, sin tener que vivir del cine laboralmente.
+        </p>
     </div>
 }
