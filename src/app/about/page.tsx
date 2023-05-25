9c32dd15e2f33d3446826bb731f6021d76b74783
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-25 19:05:03 -0300

    about page styles created

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import { Gloria_Hallelujah } from 'next/font/google'
 import AboutText from '@/components/aboutText/AboutText'
 import CvButtons from '@/components/cvButtons/CvButtons'
+import styles from "./page.module.scss"
 
 const gloria_hallelujah = Gloria_Hallelujah({
     weight: "400",
@@ -9,7 +10,7 @@ const gloria_hallelujah = Gloria_Hallelujah({
 })
 
 export default function About() {
-    return <div className={`${gloria_hallelujah.className}`}>
+    return <div className={`${styles.container} ${gloria_hallelujah.className}`}>
         <AboutText />
         <CvButtons />
     </div>
