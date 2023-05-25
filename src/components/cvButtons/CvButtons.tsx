598ba170cfdf2ea6c8eb15acc6cf4d92522b66ae
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-05-25 20:19:03 -0300

    cv buttons ready

--- src/components/cvButtons/CvButtons.tsx
+++ src/components/cvButtons/CvButtons.tsx
@@ -1,6 +1,8 @@
+import styles from "./CvButtons.module.scss"
+
 export default function CvButtons() {
-    return <div>
-        <a href="https://docs.google.com/document/d/1tYuUHR9nnUZ7a7ZTfLvWgqaefmPHWiyZnlLKZb9-KtE/edit#" target="_blank">CV (Español)</a>
-        <a href="https://docs.google.com/document/d/12ctMoxiKVPbgcJWsULJBSeWU2VbTTvwXGfzQBLoAPu4/edit#" target="_blank">CV (English)</a>
+    return <div className={styles.container}>
+        <a className={styles.btn} href="https://docs.google.com/document/d/1tYuUHR9nnUZ7a7ZTfLvWgqaefmPHWiyZnlLKZb9-KtE/edit#" target="_blank">CV (Español)</a>
+        <a className={styles.btn} href="https://docs.google.com/document/d/12ctMoxiKVPbgcJWsULJBSeWU2VbTTvwXGfzQBLoAPu4/edit#" target="_blank">CV (English)</a>
     </div>
 }
