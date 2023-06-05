27a75f238284d221718c4795b7697869d759b47f
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-06-05 11:05:16 -0300

    set up font for project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,14 @@
-export default function Projects() {
-    return <>
-    <h2>projects page</h2>
-    </>
+import { Poppins } from "next/font/google"
+import Projects from "@/components/projects/projects"
+
+const poppins = Poppins({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+})
+
+export default function ProjectsPage() {
+    return <div className={`${poppins.className}`}>
+        <Projects />
+    </div>
 }
---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+    return <section>
+        <h1>hola</h1>
+    </section>
+}
